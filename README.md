94b2ce14e0bdc30ab83e5bbd9cdc334a2e20bf96
KallyDev <kallydev@gmail.com>
2022-02-21 13:00:33 +0800

    init: create project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Market Monitor Bot
+
+![GitHub last commit](https://img.shields.io/github/last-commit/kallydev/market-monitor-bot?style=flat-square)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/kallydev/market-monitor-bot?style=flat-square)
+![GitHub license](https://img.shields.io/github/license/kallydev/market-monitor-bot?style=flat-square)
+
+## Introduction
+
+Cryptocurrency market monitor bot.
+
+## License
+
+[MIT License](LICENSE).
---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import Web3 from "web3";
+import {AbiItem} from 'web3-utils'
+import {PancakeSwapRouterABI, PancakeSwapRouterAddress} from "./contract/pancakeswap/router";
+import {CronJob} from "cron";
+import {TONCOINAddress} from "./token/toncoin";
+import {USDTAddress} from "./token/usdt";
+import {Client as PostgresClient} from "pg";
+import "dotenv/config";
+import {Telegraf} from "telegraf";
+
+const main = async () => {
+    const postgresClient = new PostgresClient({
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+    });
+    await postgresClient.connect();
+
+    const telegramBot = new Telegraf(String(process.env.TELEGRAM_BOT_TOKEN));
+
+    const web3 = new Web3(process.env.WEB3_ENDPOINT || "https://bsc-dataseed.binance.org/");
+    const pancakeSwapRouterContract = new web3.eth.Contract(PancakeSwapRouterABI as AbiItem[], PancakeSwapRouterAddress);
+
+    const cronJob = new CronJob("0 * * * *", async () => {
+        pancakeSwapRouterContract.methods.getAmountsOut(1, [TONCOINAddress, USDTAddress]).call({}, (error: any, result: number[]) => {
+            telegramBot.telegram.sendMessage(Number(process.env.TELEGRAM_CHAT_ID), `*TONCOIN/USDT*\n\n*Price*\n\`$${result[1] / 1e9}\`\n\n*Change of 1H*\n\`-\``, { parse_mode: "MarkdownV2" })
+        });
+    });
+    cronJob.start();
+}
+
+main().then();
